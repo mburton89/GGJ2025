ed93fe7553e7496cfc48f474da22250e943ccbdf
WUStudent <wustudent@webster.edu>
2025-01-24 20:55:52 -0600

    Making shit up

---
+++ Bubble 3D/Assets/Scripts/Throwing.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwing : MonoBehaviour
+{
+    [Header("Prefab Settings")]
+    public GameObject newsPaperPrefab; //Newspaper prefab
+    public Transform spawnpoint; //Spawn point og prefab
+
+
+    [Header("Throw Settings")]
+    public float throwingForce = 20f;
+    public Vector3 leftDirection = new Vector3(-1, 1, 0);
+    public Vector3 rightDirection = new Vector3(1, 1, 0);
+    public float objectLifeTime;
+    private bool canThrow;
+    public int throwDelay;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canThrow)
+        {
+            //left click
+            StartCoroutine(ThrowWithDelay(leftDirection));
+
+
+        }
+        else if (Input.GetMouseButtonDown(1) && canThrow)
+        {
+            //right click
+            StartCoroutine(ThrowWithDelay(rightDirection));
+
+
+        }
+
+    }
+
+  
+
+    IEnumerator ThrowWithDelay(Vector3 direction)
+    {
+        canThrow = false;
+
+        GameObject thrownObject = Instantiate(newsPaperPrefab, spawnpoint.position, Quaternion.identity);
+
+        Rigidbody rb = thrownObject.GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("The prefab must have a Rigidbody component!");
+            Destroy(thrownObject);
+            yield break;
+
+        }
+
+        rb.velocity = Vector3.zero;
+        rb.AddForce(direction.normalized * throwingForce, ForceMode.Impulse);
+
+        Destroy(thrownObject,objectLifeTime);
+
+        yield return new WaitForSeconds(throwDelay);
+        canThrow = true;
+
+    }
+}
