e38b8a2edb86a0339c101a465ead524511abe2b0
Cjwwolf404 <157659337+Cjwwolf404@users.noreply.github.com>
2025-01-25 17:06:28 -0600

    Main Menu
    Very simple main menu scene built with buttons for quit and start

--- Bubble 3D/Assets/Scripts/UIManager.cs
+++ Bubble 3D/Assets/Scripts/UIManager.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public TMP_Text scoreText;
-    public int currentScore; 
+    public int currentScore;
+
+    public GameObject startButton;
+    public GameObject quitButton;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //SceneManager.LoadScene(1);
+        //print("MainMenu Loaded");
         currentScore = 0;
     }
 
@@ -26,6 +33,17 @@ public class UIManager : MonoBehaviour
     {
         currentScore += points;
         scoreText.text = "Score: " + currentScore;
+    }
+
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnQuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
     }
 
 }
