f0e916e50f255b282f0070878b61de7c2a97dbbf
WUStudent <wustudent@webster.edu>
2025-01-25 19:08:25 -0600

    yep

--- Bubble 3D/Assets/Scripts/Throwing.cs
+++ Bubble 3D/Assets/Scripts/Throwing.cs
@@ -78,21 +78,24 @@ public class Throwing : MonoBehaviour
             rb.velocity = GetComponent<Rigidbody>().velocity;
         }
 
-        Vector3 throwDirection = direction.normalized * throwingForce;
+        Vector3 newThrowDirection;
 
-        // Calculate throw direction relative to the character's facing direction
-        Vector3 relativeThrowDirection = transform.TransformDirection(new Vector3(throwDirection.x, 0, throwDirection.z));
-        //Vector3 relativeThrowDirection = transform.forward * throwingForce;
-
-        // Add upward velocity to the throw direction
-        Vector3 newThrowDirection = new Vector3(relativeThrowDirection.x, upwardVelocity, relativeThrowDirection.z);
-        //Vector3 newThrowDirection = new Vector3(relativeThrowDirection.x, upwardVelocity, relativeThrowDirection.z);
+        if (direction == transform.forward)
+        {
+            Vector3 throwDirection = direction.normalized * throwingForce;
+            newThrowDirection = new Vector3(throwDirection.x, upwardVelocity, throwDirection.z);
+        }
+        else
+        {
+            Vector3 throwDirection = direction.normalized * throwingForce;
+            Vector3 relativeThrowDirection = transform.TransformDirection(new Vector3(throwDirection.x, 0, throwDirection.z));
+            newThrowDirection = new Vector3(relativeThrowDirection.x, upwardVelocity, relativeThrowDirection.z);
+        }
 
         rb.AddForce(newThrowDirection, ForceMode.Impulse);
-        //rb.AddTorque(new Vector3(newspaperTorqueX, newspaperTorqueY, newspaperTorqueZ));
-        Debug.Log("Normaized");
-        Destroy(thrownObject,objectLifeTime);
+        rb.AddForce(transform.forward * throwingForce / 2, ForceMode.Impulse);
 
+        Destroy(thrownObject,objectLifeTime);
         yield return new WaitForSeconds(throwDelay);
         canThrow = true;
     }
